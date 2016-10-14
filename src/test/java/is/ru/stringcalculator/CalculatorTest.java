26ff6a4a7371eeeca013f8de58eb7d8792f381e2
Sigurdurhelga <sigurdurhel15@ru.is>
2016-10-14 18:23:45 +0000

    added dependencies so that unit tests can grab exceptions thrown

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -3,11 +3,18 @@ package is.ru.stringcalculator;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.hamcrest.Matcher;
+
 public class CalculatorTest {
 
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
 	@Test
 	public void testEmptyString() {
@@ -38,4 +45,19 @@ public class CalculatorTest {
     public void testAddUsingNewLine(){
    		assertEquals(10, Calculator.add("5\n3\n2"));
     }
+
+    @Test
+    public void testNegativeNumberAlone(){
+    	exception.expect(IllegalArgumentException.class);
+    	exception.expectMessage("Negatives not allowed: -5");
+    	Calculator.add("-5");
+    }
+
+    @Test
+    public void testMultipleNegativeNumbers(){
+    	exception.expect(IllegalArgumentException.class);
+    	exception.expectMessage("Negatives not allowed: -5,-2");
+    	Calculator.add("-5\n3,-2");
+    }
+
 }
